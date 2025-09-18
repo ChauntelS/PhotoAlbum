0adfdc553b883e28494583fa8be72660f835cce7
ChauntelS <w0239497@campus.nscc.ca>
2025-09-18 14:27:00 -0300

    After Thursday's class

--- PhotoAlbum/Controllers/HomeController.cs
+++ PhotoAlbum/Controllers/HomeController.cs
@@ -15,10 +15,40 @@ namespace PhotoAlbum.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            //Create a list of photos
+
+            List<Photo> photoList = new List<Photo>();
+
+            Photo photo1 = new Photo();
+            photo1.Id = 1;
+            photo1.Title = "Penny";
+            photo1.Description = "My dog Penny";
+            photo1.FileName = "penny.jpg";
+            photo1.Creation = new DateTime(2025,09,18, 13,51,00);
+
+            Photo photo2 = new Photo();
+            photo2.Id = 2;
+            photo2.Title = "Ozzy";
+            photo2.Description = "My cat Ozzy";
+            photo2.FileName = "Ozzy.jpg";
+            photo2.Creation = DateTime.Now;
+
+            photoList.Add(photo1);
+            photoList.Add(photo2);
+
+            _logger.Log(LogLevel.Information,"Number of Photos: " + photoList.Count);
+
+            //Pass the list of photos to the view
+
+            return View(photoList);
         }
 
         public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Hello()
         {
             return View();
         }
